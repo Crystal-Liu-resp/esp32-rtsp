45b18d93e605bf0332de138c8201fd75faf89a37
TDA2030 <1932489836@qq.com>
2021-05-24 01:22:26 +0800

    add g711 media

---
+++ src/media_g711a.cpp
@@ -0,0 +1,66 @@
+﻿
+
+#include "G711ASource.h"
+#include <cstdio>
+#include <chrono>
+
+
+G711ASource::G711ASource()
+{
+	payload_    = 8;
+	media_type_ = PCMA;
+	clock_rate_ = 8000;
+}
+
+G711ASource* G711ASource::CreateNew()
+{
+    return new G711ASource();
+}
+
+G711ASource::~G711ASource()
+{
+	
+}
+
+string G711ASource::GetMediaDescription(uint16_t port)
+{
+	char buf[100] = {0};
+	sprintf(buf, "m=audio %hu RTP/AVP 8", port);
+	return string(buf);
+}
+	
+string G711ASource::GetAttribute()
+{
+    return string("a=rtpmap:8 PCMA/8000/1");
+}
+
+bool G711ASource::HandleFrame(MediaChannelId channel_id, AVFrame frame)
+{
+	if (frame.size > MAX_RTP_PAYLOAD_SIZE) {
+		return false;
+	}
+
+	uint8_t *frame_buf  = frame.buffer.get();
+	uint32_t frame_size = frame.size;
+
+	RtpPacket rtp_pkt;
+	rtp_pkt.type = frame.type;
+	rtp_pkt.timestamp = frame.timestamp;
+	rtp_pkt.size = frame_size + 4 + RTP_HEADER_SIZE;
+	rtp_pkt.last = 1;
+
+	memcpy(rtp_pkt.data.get()+4+RTP_HEADER_SIZE, frame_buf, frame_size);
+
+	if (send_frame_callback_) {
+		send_frame_callback_(channel_id, rtp_pkt);
+	}
+
+	return true;
+}
+
+uint32_t G711ASource::GetTimestamp()
+{
+	auto time_point = chrono::time_point_cast<chrono::microseconds>(chrono::steady_clock::now());
+	return (uint32_t)((time_point.time_since_epoch().count()+500)/1000*8);
+}
+
---
+++ src/media_g711a.h
@@ -0,0 +1,47 @@
+﻿
+
+#ifndef _MEDIA_G711A_H_
+#define _MEDIA_G711A_H_
+
+#include "MediaSource.h"
+#include "rtp.h"
+
+
+
+class G711ASource : public MediaSource
+{
+public:
+	static G711ASource* CreateNew();
+	virtual ~G711ASource();
+
+	uint32_t GetSampleRate() const
+	{ return samplerate_; }
+
+	uint32_t GetChannels() const
+	{ return channels_; }
+
+	virtual std::string GetMediaDescription(uint16_t port=0);
+
+	virtual std::string GetAttribute();
+
+	bool HandleFrame(MediaChannelId channel_id, AVFrame frame);
+
+	static uint32_t GetTimestamp();
+
+private:
+	G711ASource();
+
+	uint32_t samplerate_ = 8000;   
+	uint32_t channels_ = 1;       
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
